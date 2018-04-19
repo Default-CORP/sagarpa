17148dd9dd9d1245077fedc5ef111a5688e66d54
d3f4ult0 <addh2.0.1@gmail.com>
2018-04-18 21:27:28 -0500

    Primera prueba de sitio

---
+++ views/index.view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Prueba de sitio</title>
+  </head>
+  <body>
+    <h1>Primera prueba para proyecto ITESA-CADERA Apan</h1>
+  </body>
+</html>
